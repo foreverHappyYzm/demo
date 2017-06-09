def109943a45e45fed377fbf56d7ac8889b26edd
foreverHappyYzm <896361820@qq.com>
2017-06-09 23:33:51 +0800

    Create guanzhu.lua

--- guanzhu.lua
+++ guanzhu.lua
@@ -19,15 +19,6 @@ if args.action == "setfocus" then
   local time = ngx.time()     
   local setres, err = ssdbMethod.zset_data(selfzname, focusedNum, time) 
   local setres, err = ssdbMethod.zset_data(fanszname, self, time)
-
-  local msgT = {sid = args.alarm, rid = v, data = "有人点赞", gps={H = "0", W = "0"}, cmd = "4",
-                mtype = "8", type = "6"}
-    
-  local sendMessageT, msg_maxT = commonUtils.makeMessage(msgT)
-  local setmsgT = commonUtils.setMessage(sendMessageT, msg_maxT[1], focusedNum)
-  local sendRes, err  = commonUtils.sendMessage(setmsgT, focusedNum)
-  ngx.say("error", err)
-  ngx.exit(200)
 end
 
 if args.action == "delfocus" then       
