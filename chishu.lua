8ff30884caea7283186eeca15fec5835472d05cc
foreverHappyYzm <896361820@qq.com>
2017-06-09 22:10:48 +0800

    Create chishu.lua

--- chishu.lua
+++ chishu.lua
@@ -13,25 +13,25 @@ local db = ssdb.get_ssdb_db()
 local fangfa = ngx.req.get_method()
 --判断是否为get请求
 if fangfa == "get" then
-  --获取geturi
-  local args = ngx.req.get_uri_args()
-  --获取发帖信息
-  local neirong = db:get(args.postid)
-  --转换成表
-  local tb = cjson.decode(neirong)
-  --判断操作人是否为发帖人
-  if args.alarm ~= tb.alarm then
-    --不是帖子浏览数加1
-    tb.read_count = tb.read_count + 1
-    --将新表转换成字符串形式
-    local tb = cjson.encode(tb)
-    --插入ssdb
-    local ok,err = db:set(tb.postid,tb)
-      if not ok then
-        ngx.say("failed to set postid:",err)
-        return
-      end
-   end
+    --获取geturi
+    local args = ngx.req.get_uri_args()
+    --获取发帖信息
+    local neirong = db:get(args.postid)
+    --转换成表
+    local tb = cjson.decode(neirong)
+    --判断操作人是否为发帖人
+    if args.alarm ~= tb.alarm then
+        --不是帖子浏览数加1
+        tb.read_count = tb.read_count + 1
+        --将新表转换成字符串形式
+        local tb = cjson.encode(tb)
+        --插入ssdb
+        local ok,err = db:set(tb.postid,tb)
+        if not ok then
+            ngx.say("failed to set postid:",err)
+            return
+        end
+    end
 end
 --关闭ssdb连接
 ssdb.close_ssdb_db()
