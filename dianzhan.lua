77ed1aedca8bec30401b43d4e01ed1faa89cf196
foreverHappyYzm <896361820@qq.com>
2017-06-09 23:52:47 +0800

    Create dianzhan.lua

--- dianzhan.lua
+++ dianzhan.lua
@@ -16,32 +16,7 @@ local zname = args.postid .. "_praise"
 
 local time = ngx.time() 
 
-local key = args.alarm 
-
-if args.action == "setpraise" then 
-    local delCommentIdRes, err = ssdbMethod.zset_data(zname,key,time) 
-    if not delCommentIdRes then
-       ngx.say("err:", err)   --日志
-       ngx.exit(200)
-    end
-    
-    local getCount, err = ssdbMethod.zsize(zname) 
-    local getFans, err = ssdbMethod.zkeys(zname,"","","",getCount) 
-    if not getFans then
-        ngxsay("error:", err) 
-        ngx.exit(200)
-    end
-    
-    if next(getFans) then  
-        for _,v in pairs (getFans) do 
-            local msgT = {sid = args.alarm, rid = v, data = "有人点赞", gps={H = "0", W = "0"}, cmd = "4",
-                mtype = "8", type = "6"}           
-            local sendMessageT, msg_maxT = commonUtils.makeMessage(msgT)                     
-            local setmsgT = commonUtils.setMessage(sendMessageT, msg_maxT[1],v )         
-            local sendRes, err  = commonUtils.sendMessage(setmsgT, v)
-        end
-    end
-end
+local key = args.alarm
 
 if args.action == "delpraise" then  
     local delPraiseRes, err = ssdbMethod.zdel(zname,key)   
@@ -49,4 +24,32 @@ if args.action == "delpraise" then
          ngx.say("error:", err)  
          ngx.exit(200)
     end
+    
+    return
+end
+
+if args.action ~= "setpraise" then 
+    return ngx.exit(403)
+end
+
+local delCommentIdRes, err = ssdbMethod.zset_data(zname,key,time) 
+    if not delCommentIdRes then
+       ngx.say("err:", err)   --日志
+       ngx.exit(200)
+    end
+    
+local getCount, err = ssdbMethod.zsize(zname)
+
+local getFans, err = ssdbMethod.zkeys(zname,"","","",getCount) 
+if not getFans then
+        ngxsay("error:", err) 
+        ngx.exit(200)
+    end
+     
+for _,v in pairs (getFans) do 
+   local msgT = {sid = args.alarm, rid = v, data = "有人点赞", gps={H = "0", W = "0"}, cmd = "4",
+                mtype = "8", type = "6"}           
+   local sendMessageT, msg_maxT = commonUtils.makeMessage(msgT)                     
+   local setmsgT = commonUtils.setMessage(sendMessageT, msg_maxT[1],v )         
+   local sendRes, err  = commonUtils.sendMessage(setmsgT, v)
 end
