ad7d30c32961d9f79137b6456bd55647c7103d32
foreverHappyYzm <896361820@qq.com>
2017-06-09 23:09:08 +0800

    Create guanzhu.lua

--- guanzhu.lua
+++ guanzhu.lua
@@ -17,8 +17,8 @@ local selfzname = "focus_" .. self
 local fanszname = focusedNum.."_focus"      
 if args.action == "setfocus" then       
   local time = ngx.time()     
-  local setres,err = ssdbMethod.zset_data(selfzname,focusedNum,time) 
-  local setres,err = ssdbMethod.zset_data(fanszname,self,time)
+  local setres, err = ssdbMethod.zset_data(selfzname, focusedNum, time) 
+  local setres, err = ssdbMethod.zset_data(fanszname, self, time)
 
   local msgT = {}
   msgT.sid = args.alarm
@@ -32,15 +32,15 @@ if args.action == "setfocus" then
   msgT.mtype = "9"
   msgT.type = "G"
     
-  local sendMessageT,msg_maxT = commonUtils.makeMessage(msgT)
-  local setmsgT = commonUtils.setMessage(sendMessageT,msg_maxT[1],focusedNum)
+  local sendMessageT, msg_maxT = commonUtils.makeMessage(msgT)
+  local setmsgT = commonUtils.setMessage(sendMessageT, msg_maxT[1], focusedNum)
   local sendRes, err  = commonUtils.sendMessage(setmsgT, focusedNum)
-  ngx.say("error",err)
+  ngx.say("error", err)
   ngx.exit(200)
 end
 
 if args.action == "delfocus" then       
     local time = ngx.time()             
-    local delres,err = ssdbMethod.zdel_data(selfzname,focusedNum)
-    local delres,err = ssdbMethod.zdel_data(fanszname,self)
+    local delres, err = ssdbMethod.zdel_data(selfzname, focusedNum)
+    local delres, err = ssdbMethod.zdel_data(fanszname, self)
 end
