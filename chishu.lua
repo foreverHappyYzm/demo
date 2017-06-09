b5a21ef6dd68fdf8d09e8601a6a3c52ccd093276
foreverHappyYzm <896361820@qq.com>
2017-06-09 21:44:21 +0800

    Create chishu.lua

--- chishu.lua
+++ chishu.lua
@@ -4,22 +4,34 @@ local dbMethod = require "gcs1.db.method"
 local utils = require "gcs1.utils"
 local commonUtils   = require "gcs1.account.common_utils"
 local ssdb = require "gcs1.ssdb.utils"
+--共享内存
 local config = ngx.shared.gcs_config1
 local userInfo = ngx.shared.userinfo1
+--实质化对象
 local db = ssdb.get_ssdb_db()
+--获取请求方法
 local fangfa = ngx.req.get_method()
+--判断是否为get请求
 if fangfa == "get" then
-local args = ngx.req.get_uri_args()
-local neirong = db:get(args.postid)
-local tb = cjson.decode(neirong)
-if args.alarm ~= tb.alarm then
-tb.read_count = tb.read_count + 1
-local tb = cjson.encode(tb)
-local ok,err = db:set(tb.postid,tb)
-if not ok then
-ngx.say("failed to set postid:",err)
-return
+  --获取geturi
+  local args = ngx.req.get_uri_args()
+  --获取发帖信息
+  local neirong = db:get(args.postid)
+  --转换成表
+  local tb = cjson.decode(neirong)
+  --判断操作人是否为发帖人
+  if args.alarm ~= tb.alarm then
+    --不是帖子浏览数加1
+    tb.read_count = tb.read_count + 1
+    --将新表转换成字符串形式
+    local tb = cjson.encode(tb)
+    --插入ssdb
+    local ok,err = db:set(tb.postid,tb)
+      if not ok then
+        ngx.say("failed to set postid:",err)
+        return
+      end
+   end
 end
-end
-end  
+--关闭ssdb连接
 ssdb.close_ssdb_db()
