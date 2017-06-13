e45753e333d7514fc5450bdc46983fab89c6af81
foreverHappyYzm <896361820@qq.com>
2017-06-13 17:10:01 +0800

    Create contrast.lua

--- contrast.lua
+++ contrast.lua
@@ -1,14 +1,10 @@
 local method = ngx.req.get_method()
 
-local args
-if method == ngx.HTTP_GET then
-    args = ngx.req.get_uri_args()
-elseif method == ngx.HTTP_POST then
-    ngx.req.read_body()
-    args = ngx.req.get_post_args()
-else
---boolean
+if method ~= ngx.HTTP_GET then
+    return ngx.exit(403)
 end
+
+local args = ngx.req.get_uri_args()
 
 if not args then
     ngx.say("没有数据")
@@ -21,9 +17,10 @@ local t = args.t
 
 local time = ngx.time()
 
-local http = ngx.var.host_name
-local path = ngx.var.request_uri
-local url = http..path
+--local http = ngx.var.host_name
+--local path = ngx.var.request_uri
+--local url = http..path
+local url = ngx.var.uri
 local md5 = ngx.md5("secred"..url..t)
 
 if t < time then
